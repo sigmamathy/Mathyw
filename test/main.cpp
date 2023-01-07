7279a3f3cb8aedd9332d5aedd4e63fb063382b3a
sigmamathy <makgsum@gmail.com>
2023-01-07 20:35:34 +0800

    Remove developer_menu (deprecated)

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,10 @@
 #include <Mathyw/all.hpp>
 #include <filesystem>
 
-void EventCallback(Mathyw::Window& window, Mathyw::Event const& e, Mathyw::DevMenu* menu)
+void EventCallback(Mathyw::Window& window, Mathyw::Event const& e)
 {
 	if (Mathyw::EventCast<Mathyw::WindowClosedEvent>(e))
 		window.Close();
-	menu->EventCall(e);
 }
 
 int main(int argc, char** argv)
@@ -13,21 +12,11 @@ int main(int argc, char** argv)
 	Mathyw::Window window(1600, 900, "Hello World", Mathyw::WindowDefault ^ Mathyw::WindowResizable);
 	window.Vsync(true);
 
-	std::string root = "../../../../test/";
-	Mathyw::Font arial(root + "Arial.ttf", 30);
-	auto projection = Mathyw::OrthogonalProjection(window.Size());
-
-	Mathyw::DevMenu menu(window, "Favourite", arial);
-	float x = 15.0f;
-	menu.AddFloatScrollBarField("Hello", x, 10, 30);
-
-	window.EventCallback(EventCallback, &menu);
+	window.EventCallback(EventCallback);
 
 	while (window.Active())
 	{
 		window.Clear(Mathyw::Black);
-
-		menu.Render(projection);
 
 		window.Update();
 	}
